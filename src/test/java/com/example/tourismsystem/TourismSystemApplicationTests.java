a4badd049d3c600224e92891179b4a99fc9c2e67
AlbinoTugboat <thessgamezone@gmail.com>
2025-12-13 12:02:04 +0300

    yet another hotfix

--- src/test/java/com/example/tourismsystem/TourismSystemApplicationTests.java
+++ src/test/java/com/example/tourismsystem/TourismSystemApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.tourismsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Эта строка загрузит application-test.properties
 class TourismSystemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Тест проверяет, загружается ли контекст приложения
+    }
 }
